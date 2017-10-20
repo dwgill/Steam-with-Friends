11651b42492c12cc2a0950f01b848a4852cf8c98
Kevin Barcelon <kbarcelon@nexient.com>
2017-10-20 19:59:31 -0400

    finish build scripts

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -11,23 +11,70 @@ module.exports = function(grunt) {
             }
           }
         },
+        less: {
+          default:{
+            files:[
+              {
+                expand:false,
+                src: 'app/styles/app.less',
+                dest:'dist/app.css'
+              }
+            ]
+          }
+        },
+        clean: {
+          dist: {
+            files: [
+              {
+                dot: true,
+                src: ['dist']
+              }
+            ]
+          }
+        },
         watch: {
           files: ['<%= jshint.files %>'],
           tasks: ['jshint']
         },
         concat: {
           options: {
-            separator: ';',
+            sourceMap:true
           },
-          dist: {
-            src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
-            dest: 'dist/built.js',
+          vendor: {
+            src: [
+              'app/bower_components/*/*.min.js',
+
+            ],
+            dest: 'dist/vendor.js',
+          },
+          source: {
+            src: [
+              'app/app.js',
+
+            ],
+            dest: 'dist/source.js',
           },
         },
+        copy:{
+          dist:{
+            files: [{
+              expand:true,
+              dot:true,
+              cwd: './app',
+              dest: 'dist',
+              src: [
+                '**/*.{ico,png,txt,html,json,woff,ttf,woff2}',
+                '.htaccess',
+                '!bower_components/**/*',
+                'bower_components/**/*.{woff,ttf,woff2}',
+                '!**/*.css'
+              ]
+            }]
+          }
+        }
+
       });
-    
-      grunt.loadNpmTasks('grunt-contrib-jshint');
-      grunt.loadNpmTasks('grunt-contrib-watch');
+  
     
       grunt.registerTask('default', ['jshint']);
       grunt.registerTask('build', function(){
