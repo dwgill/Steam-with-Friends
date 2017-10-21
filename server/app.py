0b19ccf877dccfc9fca5a451f30975c676b929ca
Kevin Barcelon <kbarcelon@nexient.com>
2017-10-21 00:28:58 -0400

    added flask cors

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import util
 app = Flask(__name__)
-
+cors = CORS(app, resources={r"/get-games*": {"origins": "*"}})
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
